1067f3b7f7df8c7a752519f24b075b528436b11c
Anis Timur Shayk <anistimurshayk@gmail.com>
2025-12-22 20:23:02 +0530

    Initialise new repo fr

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int luckynum = 10;
+    cout << "Hello World!" << endl << luckynum << endl;
+    cout <<"Enter your lucky number: " << endl;
+    cin >> luckynum;
+    cout << "Your new lucky number is " << luckynum << endl;
+    return 0;
+}
